df8455a3fa33c14a563ee1b639503257947dad7c
Steingrim Dovland <steingrd@ifi.uio.no>
2009-10-25 12:49:43 +0100

    simplified shortcut functions, no longer supports context_instance

--- django_plist/__init__.py
+++ django_plist/__init__.py
@@ -4,18 +4,10 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def render_array(iterable, context_instance=None):
-    if not context_instance:
-        context = RequestContext({'array': iterable})
-    else:
-        context['array'] = iterable
-    return render_to_response('django_plist/array.plist', 
-        context_instance=context)
+def render_array(iterable):
+    context = {'array': iterable}
+    return render_to_response('django_plist/array.plist', context)
     
-def render_dictionary(dictionary, context_instance=None):
-    if not context_instance:
-        context = RequestContext({'dictionary': dictionary})
-    else:
-        context['dictionary'] = dictionary
-    return render_to_response('django_plist/dictionary.plist', 
-        context_instance=context)
+def render_dictionary(dictionary):
+    context = {'dictionary': dictionary}
+    return render_to_response('django_plist/dictionary.plist', context)
