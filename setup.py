3dca5b1bab064b476e2b64f7aa067cc0a001d237
Steingrim Dovland <steingrd@ifi.uio.no>
2009-10-25 18:18:27 +0100

    added all packages below django_plist including templates

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@
 
 from distutils.core import setup
 
+django_plist_packages = [
+    'django_plist',
+    'django_plist.views',
+    'django_plist.templatetags',
+]
+
 setup(name='django-plist',
       version='0.1',
       author='Steingrim Dovland',
       author_email='steingrd@ifi.uio.no',
       url='http://steingrd.github.com/django-plist/',
-      packages=['django_plist'],
+      packages=django_plist_packages,
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Framework :: Django',
