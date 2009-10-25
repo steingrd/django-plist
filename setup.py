8c0a9d063895c9d9f5234c854374e7cd02e9646b
Steingrim Dovland <steingrd@ifi.uio.no>
2009-10-25 21:01:21 +0100

    template directory as package_data to fix bdist building

--- setup.py
+++ setup.py
@@ -9,12 +9,18 @@ django_plist_packages = [
     'django_plist.templatetags',
 ]
 
+django_plist_templates = [
+    'templates/django_plist/array.plist',
+    'templates/django_plist/dictionary.plist'
+]
+
 setup(name='django-plist',
-      version='0.1',
+      version='0.2',
       author='Steingrim Dovland',
       author_email='steingrd@ifi.uio.no',
       url='http://steingrd.github.com/django-plist/',
       packages=django_plist_packages,
+      package_data={'django_plist': django_plist_templates},
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Framework :: Django',
